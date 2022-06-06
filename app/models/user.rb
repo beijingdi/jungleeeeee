10e12b0ba9bcd18530e5b8f776d582e91e79e8f7
Di <dwu233@gmail.com>
2022-06-06 20:26:04 +0000

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, confirmation: true
+  validates :password_confirmaion, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
