665d76282c85b9709c1afc5781e99aa5e0aadd82
Di <dwu233@gmail.com>
2022-06-06 22:51:47 +0000

    add required tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
   validates :password, confirmation: true
-  validates :password_confirmaion, presence: true
+  validates :password, length:{ minimum: 8 }
+  validates :password_confirmation, presence: true
   validates :email, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-
+  before_save { self.email = email.downcase }
+  def self.authenticate_with_credentials(params)
+    email = params[:email].strip.downcase
+    @user = User.find_by_email(email)
+    @user.authenticate(params[:password]) ? @user :nil
+    # If the user exists AND the password entered is correct.
+  end
 end
+
+
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "creates the user with valid inputs" do
+      goodUser = User.new(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWoody")
+      assert goodUser.save!
+    end
+    it "won't create the user when the e-mail exists" do
+      goodUser = User.create(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWoody")
+      goodUser.save
+      sameEmailUser = User.new(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWoody")
+      assert_not sameEmailUser.save!
+    end
+    it "won't create the user when password confirmation doesn't match" do
+      wrongPasswordUser = User.new(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWooody")
+      assert_not wrongPasswordUser.save!
+    end
+    it "won't create the user when the password has less than 8 characters" do
+      shortPasswordUser = User.new(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luv", :password_confirmation => "luv")
+      assert_not wrongPasswordUser.save!
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it "returns the user with correct credentials" do
+      goodUser = User.create(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWoody")
+      expect(User.authenticate_with_credentials(:email =>"dogdogdog@dog.com", :password=> "luvWoody")).to eq(goodUser)
+    end
+    it "returns nil with wrong credentials" do
+      goodUser = User.create(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWoody")
+      expect(User.authenticate_with_credentials(:email =>"dogdogdog@dog.com", :password=> "luvW0oody")).to eq(nil)
+    end
+    it "should ignore the white spaces at the beginning of user email" do
+      goodUser = User.create(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWoody")
+      expect(User.authenticate_with_credentials(:email =>"    dogdogdog@dog.com", :password=> "luvWoody")).to eq(goodUser)
+    end
+    it "should ignore case of the user email when authenticating" do
+      goodUser = User.create(:first_name => "Michr", :last_name => "DingDong", :email => "dogdogdog@dog.com", :password => "luvWoody", :password_confirmation => "luvWoody")
+      expect(User.authenticate_with_credentials(:email =>"DogDogDog@dog.com", :password=> "luvWoody")).to eq(goodUser)
+    end
+  end
 end
